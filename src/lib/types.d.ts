508847124ab57f547b7314a1237320c66e45348e
realmbs <119774290+realmbs@users.noreply.github.com>
2025-05-16 12:24:20 -0500

    completed data.json, created types.d.ts

---
+++ src/lib/types.d.ts
@@ -0,0 +1,45 @@
+export interface Contact {
+  email: string;
+  linkedin: string;
+  github: string;
+  website: string;
+  location: string;
+}
+
+export interface EducationEntry {
+  degree: string;
+  institution: string;
+  location: string;
+  honors: string;
+  gpa: string;
+  startDate: string;
+  endDate: string;
+}
+
+export interface CertificationEntry {
+  name: string;
+  authority: string;
+  date: string;
+  url: string;
+}
+
+export interface CertificationInProgressEntry {
+  name: string;
+  date: string;
+}
+
+export interface SkillEntry {
+  category: string;
+  skills: string[];
+}
+
+export interface Resume {
+  name: string;
+  headline: string;
+  contact: Contact;
+  summary: string;
+  education: EducationEntry[];
+  certifications: CertificationEntry[];
+  certificationsInProgress: CertificationInProgressEntry[];
+  skills: SkillEntry[];
+}
