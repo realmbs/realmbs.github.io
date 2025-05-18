71859a7f6b166ce7a03d39a5ad5a162961d83903
realmbs <119774290+realmbs@users.noreply.github.com>
2025-05-17 19:43:27 -0500

    working on darkmode

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,46 +1,6 @@
-export interface EducationEntry {
-  school: string;
-  location: string;
-  degree: string;
-  gpa: string;
-  honors: string;
-  startDate: string;
-  endDate: string;
-}
-
-export interface ProjectEntry {
-  name: string;
-  date: string;
-  description: string;
-}
-
-export interface CertificationEntry {
-  name: string;
-  issuer: string;
-  date: string;
-  url: string;
-}
-
-export interface CertificationInProgressEntry {
-  name: string;
-  issuer: string;
-  date: string;
-}
-
-export interface SkillEntry {
-  category: string;
-  skills: string[];
-}
-
 export interface SiteData {
   name: string;
   title: string;
   location: string;
-  linkedin: string;
-  summary: string;
-  education: EducationEntry[];
-  projects: ProjectEntry[];
-  certifications: CertificationEntry[];
-  certificationsInProgress: CertificationInProgressEntry[];
-  skills: SkillEntry[];
+  linkedin: string
 }
