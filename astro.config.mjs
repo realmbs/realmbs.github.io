92a076488f387920879aa205b34eb41996999265
realmbs <119774290+realmbs@users.noreply.github.com>
2025-05-24 14:18:19 -0500

    .........

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://realmbs.github.io',
-  base: '/portfolio',
   output: 'static',
   build: {
     assets: 'assets'
