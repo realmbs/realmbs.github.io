3f71f3bb280f8a58975c70d509f9693c2a17417b
realmbs <119774290+realmbs@users.noreply.github.com>
2025-05-24 14:10:38 -0500

    ......

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sass from '@astrojs/sass';
 
 export default defineConfig({
-  integrations: [sass()],
   site: 'https://realmbs.github.io',
   base: '/portfolio',
   output: 'static',
