f22cb9002aff5c7e25ed5ed5368dbb965cf10e03
realmbs <119774290+realmbs@users.noreply.github.com>
2025-05-31 12:12:23 -0500

    created types/data.ts

---
+++ src/types/data.ts
@@ -0,0 +1,63 @@
+export interface Personal {
+  name: string;
+  title: string;
+  phone: string;
+  email: string;
+  location: string;
+  summary: string;
+}
+
+export interface Education {
+  institution: string;
+  location: string;
+  degree: string;
+  gpa: number;
+  honors: string[];
+  startDate: string;
+  endDate: string;
+  status: 'current' | 'completed';
+}
+
+export interface Project {
+  title: string;
+  date: string;
+  description: string;
+  technologies: string[];
+}
+
+export interface Certification {
+  name: string;
+  date?: string;
+  issuer?: string;
+  expected?: string;
+  status?: string;
+}
+
+export interface Certifications {
+  completed: Certification[];
+  inProgress: Certification[];
+}
+
+export interface Skills {
+  programming: string[]
+  security: string[]
+  networking: string[]
+  frameworks: string[]
+  cryptography: string[]
+  grc: string[]
+  soft: string[]
+}
+
+export interface Social {
+  github: string;
+  linkedin: string;
+}
+
+export interface SiteData {
+  personal: Personal;
+  education: Education[];
+  projects: Project[];
+  certifications: Certifications;
+  skills: Skills;
+  social: Social;
+}
